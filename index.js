bebcefc27a8a4a844f683fe243b5bb763d157e61
vintageclias <vintageclias@gmail.com>
2024-10-20 23:56:50 -0700

    adding behavior

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Your code here
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        return `${this.name} says meow!`;
+    }
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        if (this.sex === 'male') {
+            return `It's me! ${this.name}, the parrot!`;
+        } else {
+            return `${this.name} says squawk!`;
+        }
+    }
+}
+
+// Example usage:
+const cat = new Cat('Whiskers', 'male');
+console.log(cat.speak()); // Whiskers says meow!
+
+const dog = new Dog('Rex', 'male');
+console.log(dog.speak()); // Rex says woof!
+
+const bird1 = new Bird('Tweety', 'female');
+console.log(bird1.speak()); // Tweety says squawk!
+
+const bird2 = new Bird('Polly', 'male');
+console.log(bird2.speak()); // It's me! Polly, the parrot!
